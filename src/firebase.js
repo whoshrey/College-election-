8b997949113fb285c5e5f9f91f3857f52d464f5a
whoshrey <shreyajha776@gmail.com>
2025-07-21 18:54:53 +0530

    Initial commit

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,13 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAAwrZAhVfm4wIuQt-5NHLYFHjZmI_Pf0E",
-    authDomain: "college-election-system.firebaseapp.com",
-    projectId: "college-election-system",
-    storageBucket: "college-election-system.firebasestorage.app",
-    messagingSenderId: "928660193324",
-    appId: "1:928660193324:web:5a687613207397ef021189",
-    measurementId: "G-3GKEM4JJZH"
+  apiKey: "AIzaSyDrN0JwSdatph8ULZMpclA5NAeFI1gWZHI",
+  authDomain: "clg-election-dc5ac.firebaseapp.com",
+  projectId: "clg-election-dc5ac",
+  storageBucket: "clg-election-dc5ac.firebasestorage.app",
+  messagingSenderId: "563048748424",
+  appId: "1:563048748424:web:948242662033674537563d",
+  measurementId: "G-DZPLE3GSGV"
 };
 
 // Initialize Firebase
@@ -22,7 +22,6 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const analytics = getAnalytics(app);
-
 // For email link authentication
 auth.settings = {
   // URL for password reset and email verification actions
@@ -30,3 +29,5 @@ auth.settings = {
 };
 
 export { auth, db };
+
+
